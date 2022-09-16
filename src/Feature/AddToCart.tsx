fabb274abd025efcd7def9fcddd40e498170a539
torrcod <torreschristian811@gmail.com>
2022-09-16 18:03:29 +0800

    addtocart add disabled parameter

--- src/Feature/AddToCart.tsx
+++ src/Feature/AddToCart.tsx
@@ -2,59 +2,73 @@ import { Button, message } from "antd";
 import { BsFillCartFill } from "react-icons/bs";
 import { OLSitems } from "../Component/Page/Product/ShopProduct";
 import { UserContext } from "../hooks/UserContext";
-import './css/AddToCart.css'
+import "./css/AddToCart.css";
 
-const AddtoCart = (props:AddtoCart_props) => {
-    const userContext = UserContext()
-    const addToCart = userContext.cartItemHandler.addToCart;
-    const {size,type,shape} = props
+const AddtoCart = (props: AddtoCart_props) => {
+  const userContext = UserContext();
+  const addToCart = userContext.cartItemHandler.addToCart;
+  const { size, type, shape } = props;
 
-    const buttonHandler = () => {
-        addToCart(props.item)
-    }
+  const buttonHandler = () => {
+    addToCart(props.item);
+  };
 
-    return (
-        <Button 
-            style={{gap:'1em'}}
-            className="flex-center"
-            onClick={buttonHandler}
-            shape={shape??"round"} 
-            size={size??'large'} 
-            type={type??"primary"}
-            icon = {<BsFillCartFill/>}
-        >
-            ADD TO CART
-        </Button>
-    )
-}
+  return (
+    <Button
+      style={{ gap: "1em" }}
+      className="flex-center"
+      onClick={buttonHandler}
+      shape={shape ?? "round"}
+      size={size ?? "large"}
+      type={type ?? "primary"}
+      icon={<BsFillCartFill />}
+      disabled={props.disabled}
+    >
+      ADD TO CART
+    </Button>
+  );
+};
 
 type AddtoCart_props = {
-    item:OLSitems;
-    type?:"link" | "text" | "ghost" | "default" | "primary" | "dashed" | undefined;
-    size?:"small"|"middle"|"large";
-    shape?: "default" | "circle" | "round" | undefined
-}
+  disabled?: boolean;
+  item: OLSitems;
+  type?:
+    | "link"
+    | "text"
+    | "ghost"
+    | "default"
+    | "primary"
+    | "dashed"
+    | undefined;
+  size?: "small" | "middle" | "large";
+  shape?: "default" | "circle" | "round" | undefined;
+};
 
-export const addtoCartFunc = (item:OLSitems,cartItemList:CartItemList,updateCount:(itemId: string, payload: number) => void,addToCart: (item: OLSitems) => void) => {
-    const isExist = cartItemList.filter(e => e.item.itemId === item.itemId)
-        
-    if(isExist[0]){
-        const itemCount = isExist[0].itemCount
-        updateCount(item.itemId!,itemCount+1)
-    }else{
-        addToCart(item)
-    }
+export const addtoCartFunc = (
+  item: OLSitems,
+  cartItemList: CartItemList,
+  updateCount: (itemId: string, payload: number) => void,
+  addToCart: (item: OLSitems) => void
+) => {
+  const isExist = cartItemList.filter((e) => e.item.itemId === item.itemId);
 
-    message.success({
-        content:item.name + " is added to cart",
-        icon:<BsFillCartFill/>,
-        className:'addtocart-message'
-    });
-}
+  if (isExist[0]) {
+    const itemCount = isExist[0].itemCount;
+    updateCount(item.itemId!, itemCount + 1);
+  } else {
+    addToCart(item);
+  }
+
+  message.success({
+    content: item.name + " is added to cart",
+    icon: <BsFillCartFill />,
+    className: "addtocart-message",
+  });
+};
 
 type CartItemList = {
-    itemCount: number;
-    item: OLSitems;
-}[]
+  itemCount: number;
+  item: OLSitems;
+}[];
 
-export default AddtoCart ;
+export default AddtoCart;
