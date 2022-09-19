48b0b35c61989ed6395740e590e50be1fbe680f5
torrcod <torreschristian811@gmail.com>
2022-09-19 17:30:38 +0800

    update orderlist if orderlist from db update

--- src/FirebaseService/RealtimeDatabase.tsx
+++ src/FirebaseService/RealtimeDatabase.tsx
@@ -1,36 +1,48 @@
-import { get, push, ref, set } from "firebase/database";
+import { get, onValue, push, ref, set } from "firebase/database";
+import { Orders } from "../api/CustomType";
 import { database } from "./FirebaseConfig";
 
 export const writeDatabase = async (path: Path, itemToWrite: {}) => {
-    const dbRef = ref(database, path);
-    await set(dbRef, itemToWrite);
-}
+  const dbRef = ref(database, path);
+  await set(dbRef, itemToWrite);
+};
 
 export const pushItemKey = (path: Path) => {
-    const postListRef = ref(database, path);
-    const newPostRef = push(postListRef);
-    return newPostRef.key;
-}
+  const postListRef = ref(database, path);
+  const newPostRef = push(postListRef);
+  return newPostRef.key;
+};
 
 export const clearPath = async (path: Path) => {
-    await writeDatabase(path, {})
-}
+  await writeDatabase(path, {});
+};
 
 export const readData = async (path: ReadPath) => {
-    const dbRef = ref(database, path);
-    const readedData = (await get(dbRef)).val()
-    return readedData
-}
+  const dbRef = ref(database, path);
+  const readedData = (await get(dbRef)).val();
+  return readedData;
+};
 
-type Path = 'productlandingpage/' | 'users/' | string;
-type ReadPath = 'productlandingpage'
-    | 'users'
-    | 'onlineshop-category'
-    | 'onlineshop-items'
-    | 'offer-data'
-    | 'user-visited'
-    | 'model-data'
-    | 'classic-data'
-    | 'collection-data'
-    |'order-list'
-    ;
+export const onChangeData = (
+  path: ReadPath,
+  callback: (val: Orders[]) => void,
+  specificId?: string | number
+) => {
+  const valueRef = ref(database, specificId ? path + "/" + specificId : path);
+  onValue(valueRef, (snapshot) => {
+    callback(snapshot.val() as Orders[]);
+  });
+};
+
+type Path = "productlandingpage/" | "users/" | string;
+type ReadPath =
+  | "productlandingpage"
+  | "users"
+  | "onlineshop-category"
+  | "onlineshop-items"
+  | "offer-data"
+  | "user-visited"
+  | "model-data"
+  | "classic-data"
+  | "collection-data"
+  | "order-list";
