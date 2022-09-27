395c2ef54edc0b1f605327d55dbc5f621bbc63c6
TorrCod <torreschristian811@gmail.com>
2022-09-27 22:47:45 +0800

    About us Styled

--- src/Component/Page/AboutUs/AboutUs.tsx
+++ src/Component/Page/AboutUs/AboutUs.tsx
@@ -1,41 +1,47 @@
+import Footer from "../../Footer/Footer";
+
 function AboutUs() {
   return (
-    <div className="grid gap-5">
-      <div className="bg-white w-screen">
-        <img src="" alt="" />
+    <>
+      <div className="grid gap-5 place-items-center">
+        <div className="bg-white text-center rounded-xl w-11/12 mt-20">
+          <img className="h-96" src="" alt="" />
+        </div>
+        <div className="bg-white p-5 text-center rounded-xl w-11/12 mb-10">
+          <h1 className="text-lg">
+            <b>HBOMB STORY</b>
+          </h1>
+          <p className="text-gray-800">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies
+            consectetur ut hendrerit lorem fames. Consequat malesuada quis
+            accumsan, in convallis sit. Pharetra nulla id nec ligula diam
+            consequat. Rhoncus et sagittis in phasellus. Enim, purus tincidunt
+            netus enim massa in sociis. Pellentesque viverra sit imperdiet nisi,
+            nulla porta pharetra. Aenean eget euismod velit pharetra. Suscipit
+            condimentum ut quis morbi eu, faucibus. Accumsan, laoreet quis
+            mauris, vel molestie mattis euismod. In ut scelerisque quisque
+            feugiat augue feugiat bibendum gravida. Amet, diam netus tortor
+            lacus, at.
+          </p>
+          <p className="text-gray-800">
+            Pretium lectus a sed vulputate augue. Nunc semper hendrerit
+            elementum, sem eget. In pharetra dapibus pellentesque et magna
+            tincidunt etiam. Euismod facilisis egestas turpis scelerisque
+            blandit aliquam habitant. Nunc amet, molestie donec gravida sit
+            mattis. Pulvinar ipsum dignissim orci tortor hac eget ut nec aenean.
+          </p>
+          <p className="text-gray-800">
+            Etiam tempor vel vel porta dis etiam sagittis condimentum. Est, at
+            amet ac ut risus eget nibh in. Sed sed malesuada pellentesque lectus
+            ornare eu. Condimentum amet tristique nec fermentum, aliquet nisi,
+            adipiscing dolor dictumst. Consectetur diam mattis purus, id cras
+            nulla amet vitae ultrices. Magna ipsum luctus erat nibh sed risus.
+            Id facilisi odio adipiscing imperdiet pharetra, vitae ut.
+          </p>
+        </div>
       </div>
-      <div className="bg-white p-5 text-center">
-        <h3>
-          <b>HBOMB STORY</b>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies
-          consectetur ut hendrerit lorem fames. Consequat malesuada quis
-          accumsan, in convallis sit. Pharetra nulla id nec ligula diam
-          consequat. Rhoncus et sagittis in phasellus. Enim, purus tincidunt
-          netus enim massa in sociis. Pellentesque viverra sit imperdiet nisi,
-          nulla porta pharetra. Aenean eget euismod velit pharetra. Suscipit
-          condimentum ut quis morbi eu, faucibus. Accumsan, laoreet quis mauris,
-          vel molestie mattis euismod. In ut scelerisque quisque feugiat augue
-          feugiat bibendum gravida. Amet, diam netus tortor lacus, at.
-        </p>
-        <p>
-          Pretium lectus a sed vulputate augue. Nunc semper hendrerit elementum,
-          sem eget. In pharetra dapibus pellentesque et magna tincidunt etiam.
-          Euismod facilisis egestas turpis scelerisque blandit aliquam habitant.
-          Nunc amet, molestie donec gravida sit mattis. Pulvinar ipsum dignissim
-          orci tortor hac eget ut nec aenean.
-        </p>
-        <p>
-          Etiam tempor vel vel porta dis etiam sagittis condimentum. Est, at
-          amet ac ut risus eget nibh in. Sed sed malesuada pellentesque lectus
-          ornare eu. Condimentum amet tristique nec fermentum, aliquet nisi,
-          adipiscing dolor dictumst. Consectetur diam mattis purus, id cras
-          nulla amet vitae ultrices. Magna ipsum luctus erat nibh sed risus. Id
-          facilisi odio adipiscing imperdiet pharetra, vitae ut.
-        </p>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
