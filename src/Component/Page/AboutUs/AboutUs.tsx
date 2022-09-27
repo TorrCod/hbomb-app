ec08180ab17567d5a584f19ffa509d0866bfc980
TorrCod <torreschristian811@gmail.com>
2022-09-27 22:30:20 +0800

    add content in about page

--- src/Component/Page/AboutUs/AboutUs.tsx
+++ src/Component/Page/AboutUs/AboutUs.tsx
@@ -1,9 +1,42 @@
 function AboutUs() {
-    return (  
-        <div>
-            About Us
-        </div>
-    );
+  return (
+    <div className="grid gap-5">
+      <div className="bg-white w-screen">
+        <img src="" alt="" />
+      </div>
+      <div className="bg-white p-5 text-center">
+        <h3>
+          <b>HBOMB STORY</b>
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies
+          consectetur ut hendrerit lorem fames. Consequat malesuada quis
+          accumsan, in convallis sit. Pharetra nulla id nec ligula diam
+          consequat. Rhoncus et sagittis in phasellus. Enim, purus tincidunt
+          netus enim massa in sociis. Pellentesque viverra sit imperdiet nisi,
+          nulla porta pharetra. Aenean eget euismod velit pharetra. Suscipit
+          condimentum ut quis morbi eu, faucibus. Accumsan, laoreet quis mauris,
+          vel molestie mattis euismod. In ut scelerisque quisque feugiat augue
+          feugiat bibendum gravida. Amet, diam netus tortor lacus, at.
+        </p>
+        <p>
+          Pretium lectus a sed vulputate augue. Nunc semper hendrerit elementum,
+          sem eget. In pharetra dapibus pellentesque et magna tincidunt etiam.
+          Euismod facilisis egestas turpis scelerisque blandit aliquam habitant.
+          Nunc amet, molestie donec gravida sit mattis. Pulvinar ipsum dignissim
+          orci tortor hac eget ut nec aenean.
+        </p>
+        <p>
+          Etiam tempor vel vel porta dis etiam sagittis condimentum. Est, at
+          amet ac ut risus eget nibh in. Sed sed malesuada pellentesque lectus
+          ornare eu. Condimentum amet tristique nec fermentum, aliquet nisi,
+          adipiscing dolor dictumst. Consectetur diam mattis purus, id cras
+          nulla amet vitae ultrices. Magna ipsum luctus erat nibh sed risus. Id
+          facilisi odio adipiscing imperdiet pharetra, vitae ut.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
