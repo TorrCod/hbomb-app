e448fec2bf091821feb62b60ba677756b3a4d00f
TorrCod <torreschristian811@gmail.com>
2022-10-02 18:48:51 +0800

    about us page has now image onit

--- src/Component/Page/AboutUs/AboutUs.tsx
+++ src/Component/Page/AboutUs/AboutUs.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import { GlobalContext } from "../../../hooks/GlobalContext";
 import Footer from "../../Footer/Footer";
 
 function AboutUs() {
+  const globalContext = GlobalContext();
+  const imgSrc = globalContext.globalState.imageApi.AboutUs;
   return (
     <>
       <div className="grid gap-5 place-items-center md:py-32 py-20 grid-rows-2 md:grid-cols-2 md:grid-rows-1 md:max-w-6xl md:mx-auto">
-        <div className="bg-slate-700/50 drop-shadow-lg h-full text-center rounded-xl w-11/12 md:grid-cols-2 md:col-start-2 md:row-start-1">
-          <img className="h-full" src="" alt="" />
+        <div className="bg-slate-700/50 drop-shadow-lg h-full text-center rounded-xl w-11/12 md:grid-cols-2 md:col-start-2 md:row-start-1 overflow-hidden">
+          <img className="w-full h-full object-fill" src={imgSrc} alt="" />
         </div>
         <div className="bg-white p-5 text-center rounded-xl w-11/12 drop-shadow-lg">
           <h1 className="text-lg">
